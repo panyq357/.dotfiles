289c88b396cb836612d9c5c759a04cedd9c27883
panyq357 <panyq357@qq.com>
2025-10-23 19:45:00 +0800

    bind nvim-cmp to ctrl-x ctrl-o

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -6,29 +6,53 @@ return {
     'SirVer/ultisnips',
     'quangnguyen30192/cmp-nvim-ultisnips'
   },
---  config = function()
---
---    -- Set up nvim-cmp.
---    local cmp = require'cmp'
---    cmp.setup({
---      completion = {
---        autocomplete = false,  -- Disable auto pop up.
---      },
---      snippet = {
---        -- REQUIRED - you must specify a snippet engine
---        expand = function(args)
---          vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
---        end,
---      },
---      mapping = cmp.mapping.preset.insert({
---        ['<CR>'] = cmp.mapping.confirm({ select = true })
---      }),
---      sources = cmp.config.sources({
---        { name = 'nvim_lsp' },
---        { name = 'ultisnips' }, -- For ultisnips users.
---      }, {
---        { name = 'buffer' },
---      })
---    })
---  end
+  config = function()
+
+    -- Set up nvim-cmp.
+    local cmp = require'cmp'
+
+    -- Show completion menu and select first one.
+    -- If already showed, confirm selection and trigger again.
+    local function complete_select_first(option)
+      return function(fallback)
+        if cmp.visible() then
+          local ok = cmp.confirm(option)
+          if not ok then
+            fallback()
+          else
+            complete_select_first()
+          end
+        else
+          local ok = cmp.select_next_item(option)
+          if not ok then
+            local release = cmp.core:suspend()
+            fallback()
+            vim.schedule(release)
+          end
+        end
+      end
+    end
+
+    cmp.setup({
+      completion = {
+        autocomplete = false,  -- Disable auto pop up.
+      },
+      snippet = {
+        expand = function(args)
+          vim.fn["UltiSnips#Anon"](args.body)  -- Use UltiSnips as snippet engine.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = function(fallback) fallback() end,
+        ['<C-n>'] = function(fallback) fallback() end,
+        ['<C-x><C-o>'] = complete_select_first(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.select_next_item
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'ultisnips' }, -- For ultisnips users.
+      })
+    })
+  end
 }
