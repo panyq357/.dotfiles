51dae585a0f5470a2308bf76d755f94e624eea75
panyq357 <panyq357@qq.com>
2026-03-21 01:35:51 +0800

    use simple osc52 in neovim

---
+++ modules/ghostty/setup.bash
@@ -0,0 +1,5 @@
+source ${HOME}/.dotfiles/modules/util.bash
+
+mkdir -p "${HOME}/.config/ghostty"
+
+module_link "${HOME}/.dotfiles/modules/ghostty/config" "${HOME}/.config/ghostty/config"
--- modules/nvim/config/lua/config/clipboard.lua
+++ modules/nvim/config/lua/config/clipboard.lua
@@ -1,15 +1,21 @@
 -- Disable nvim clipboard passing to tmux.
 vim.opt.clipboard = ""
 
+
+local function osc52(text)
+  io.write("\27]52;c;" .. vim.base64.encode(text) .. "\7")
+  io.flush()
+end
+
+
+-- Put plain yank in osc52.
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     local event = vim.v.event
     if event.operator == "y" and event.regname == "" then
       -- Check nvim/share/nvim/runtime/lua/vim/ui/clipboard/osc52.lua
       -- Note: copy is a function factory.
-      local osc52_fun = require("vim.ui.clipboard.osc52").copy(event.regname)
-      osc52_fun(event.regcontents)
+      osc52(table.concat(event.regcontents, "\n"))
     end
   end,
 })
-
